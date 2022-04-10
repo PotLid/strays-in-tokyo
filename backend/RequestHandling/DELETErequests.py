6facb97357007f771a71df6c1b242e89f2faff3d
Mahdy <mahdyfer@buffalo.edu>
2022-04-10 15:55:40 -0400

    Added Minor code to handle HTTP requests types.

---
+++ backend/RequestHandling/DELETErequests.py
@@ -0,0 +1,14 @@
+import sys
+
+'''
+This method will handle DELETE requests
+
+The inputted data will be:
+    -- The TCP object from the maiin server.
+    -- A dictionary with Key-Value pairs from the Header and Body (Byte encoded already)
+'''
+
+def handle(TCP, path, data):
+
+
+    return
---
+++ backend/RequestHandling/POSTrequests.py
@@ -0,0 +1,16 @@
+import sys
+
+'''
+This method will handle the POST requests
+
+The inputted data will be:
+    -- The TCP object from the maiin server.
+    -- A dictionary with Key-Value pairs from the Header and Body (Byte encoded already)
+'''
+
+
+def handle(TCP, path, data):
+
+
+    return
+
