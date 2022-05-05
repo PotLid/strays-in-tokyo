2180bb5669dd8a02af368d33b4da372dd8522fcf
Mahdy <mahdyfer@buffalo.edu>
2022-05-04 22:56:25 -0400

    Added BE to FE connection on most pages

--- backend/RequestHandling/GETrequests.py
+++ backend/RequestHandling/GETrequests.py
@@ -16,19 +16,62 @@ The inputted data will be:
     '''
 
 def handle(TCP, path, data):
+    print("This is the GET request path: ", path, '\n')
+    print('This is the GET request data: ', data, '\n')
     # Route for the homepage, AKA the index.html 
     if path == b'/':
-        content = render_content("frontend/public/index.html")
+        content = render_content("frontend/templates/homepage.html")
         content = server.MyTCPHandler.generate_http_response(TCP, content.encode(), "text/html; charset=utf-8", "200 OK")
         TCP.request.sendall(content)
+    
+    elif b'.png' in path:
+        path = path[1:].decode()
+        Exist = os.path.exists(path)
+        if Exist:
+            openPNG = open(path, "rb")
+            readPNG = openPNG.read()
+            PNGbyteSize = os.path.getsize(path)
+            PNGresponse = 'HTTP/1.1 200 OK\r\nContent-Type: image/png; charset=utf-8\r\nX-Content-Type-Options: nosniff\r\nContent-Length: ' + str(PNGbyteSize) + '\r\n\r\n'
+            PNGresponse = PNGresponse.encode()
+            PNGresponse += readPNG
+            TCP.request.sendall(PNGresponse)
+
+    elif b'.css' in path:
+        path = path[1:].decode()
+        Exist = os.path.exists(path)
+        if Exist:
+            openCSS = open(path, "rb")
+            readCSS = openCSS.read()
+            CSSpath = path
+            CSSbyteSize = os.path.getsize(CSSpath)
+            CSSresponse = 'HTTP/1.1 200 OK\r\nContent-Type: text/css; charset=utf-8\r\nX-Content-Type-Options: nosniff\r\nContent-Length: ' + str(CSSbyteSize) + '\r\n\r\n'
+            CSSresponse += readCSS.decode()
+            TCP.request.sendall(CSSresponse.encode())
 
     elif path == b'/functions.js':
         content = render_content("functions.js")
         content = server.MyTCPHandler.generate_http_response(TCP, content.encode(), "text/javascript; charset=utf-8", "200 OK")
         TCP.request.sendall(content)
 
+    elif path == b'/login':
+        content = render_content("frontend/templates/login.html")
+        content = server.MyTCPHandler.generate_http_response(TCP, content.encode(), "text/html; charset=utf-8", "200 OK")
+        TCP.request.sendall(content)
+
+    elif path == b'/register':
+        content = render_content("frontend/templates/register.html")
+        content = server.MyTCPHandler.generate_http_response(TCP, content.encode(), "text/html; charset=utf-8", "200 OK")
+        TCP.request.sendall(content)
+
+    # The user MUST be authenticated to even access this page
+    elif path == b'/settings':
+        content = render_content("frontend/templates/settings.html")
+        content = server.MyTCPHandler.generate_http_response(TCP, content.encode(), "text/html; charset=utf-8", "200 OK")
+        TCP.request.sendall(content)
+
     elif path == b'/websocket':
         websocketHandler.handleWebSocket(TCP, data)
+
 
     elif path == b'/users':
         # GET /users
@@ -67,3 +110,4 @@ def render_content(html_filename: str):
     with open(html_filename) as html_file:
         content =  html_file.read()
         return content
+
