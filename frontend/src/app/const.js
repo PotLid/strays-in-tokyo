4ddd0a66da504bbdf9f917a05c57ccc60df1e041
Junwoo Park <JunwooDev@gmail.com>
2022-05-12 02:13:16 -0400

    remove react files and reports

--- frontend/src/app/const.js
+++
@@ -1,4 +0,0 @@
-export const URL_HOME = '/';
-export const URL_LOGIN = '/login';
-export const URL_ABOUT = '/about';
-export const URL_CHAT = '/chat';
--- frontend/src/app/ui.js
+++
@@ -1,5 +0,0 @@
-import Header from "../components/Header/Header";
-import Footer from "../components/Footer/Footer";
-import Background from "../components/Background/Background";
-
-export {Header, Footer, Background};
